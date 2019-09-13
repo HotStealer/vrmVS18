6175d6ddb01af3d66c8a0c8a6727e7f0ea6d5f54
Karl Põdra <karl.podra@khk.ee>
2019-09-13 12:37:39 +0300

    General HTML and CSS style to the Bookings base page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,16 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class IndexController
+class IndexController extends AbstractController
 {
 
     public function index()
     {
-       	return new Response(
-            '<html><body><h1>Booking App index page</h1></body></html>'
-        );
+       	return $this->render("main.html.twig");
     }
 }
 
